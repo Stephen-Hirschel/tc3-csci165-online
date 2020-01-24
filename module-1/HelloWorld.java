9348e2300f5a1139346b8d86396909b865845245
Stephen Hirschel <stephenhirschel@gmail.com>
2020-01-24 15:24:54 -0500

    lab submission for module 1

---
+++ module-1/HelloWorld.java
@@ -0,0 +1,10 @@
+// file name must match class identifier
+public class HelloWorld{
+
+    // main method is the starting point of any Java app
+    public static void main(String[] args){
+
+      //built in terminal printing utility
+      System.out.println("Hello World!");
+    } // end of main
+}// end of class
---
+++ module-1/HelloWorld2.java
@@ -0,0 +1,11 @@
+// file name must match class identifier
+public class HelloWorld2{
+
+    // main method is the starting point of any Java app
+    public static void main(String[] args){
+
+      for (String s : args)
+        System.out.println("Hello " + s + ", you are learning Java!");
+
+    } // end of main
+} // end of class
